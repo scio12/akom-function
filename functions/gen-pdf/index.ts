fe4288e82f390bf93cd107cba4c19e293870449f
Ezra Khairan Permana <ezrakhairan123@gmail.com>
2023-02-14 12:02:35 +0700

    feat: bypass auth system dengan cookie

--- functions/gen-pdf/index.ts
+++ functions/gen-pdf/index.ts
@@ -2,29 +2,57 @@ import { Handler } from "@netlify/functions";
 import chromium from "chrome-aws-lambda";
 import puppeteer from "puppeteer-core";
 
+import { requestBodySchema } from "../../lib/schema/gen-pdf";
+
 export const handler: Handler = async (event, context) => {
-  const browser = await puppeteer.launch({
-    args: chromium.args,
-    executablePath:
-      process.env.CHROMIUM_PATH || (await chromium.executablePath),
-    headless: true,
-  });
-  const page = await browser.newPage();
+  if (event.httpMethod === "POST") {
+    const parsedBody = JSON.parse(event.body);
+    const testedBody = await requestBodySchema.safeParseAsync(parsedBody);
 
-  await page.emulateMediaType("screen");
+    if (!testedBody.success)
+      return {
+        statusCode: 403,
+        body: JSON.stringify(testedBody),
+      };
 
-  const ss = await page.pdf({
-    format: "A4",
-    preferCSSPageSize: true,
-    printBackground: true,
-  });
+    const url = new URL(testedBody.data.userInfo["next-auth.callback-url"]);
 
-  await browser.close();
+    const cookies = Object.keys(testedBody.data.userInfo).map((name) => ({
+      name,
+      value: testedBody.data.userInfo[name],
+      domain: url.hostname,
+    }));
+
+    const browser = await puppeteer.launch({
+      args: chromium.args,
+      executablePath:
+        process.env.CHROMIUM_PATH || (await chromium.executablePath),
+      headless: true,
+    });
+    const page = await browser.newPage();
+
+    await page.setCookie(...cookies);
+
+    await page.emulateMediaType("screen");
+
+    const ss = await page.pdf({
+      format: "A4",
+      preferCSSPageSize: true,
+      printBackground: true,
+    });
+
+    await browser.close();
+
+    return {
+      statusCode: 200,
+      headers: { "Content-Type": "application/pdf" },
+      body: ss.toString("base64"),
+      isBase64Encoded: true,
+    };
+  }
 
   return {
-    statusCode: 200,
-    headers: { "Content-Type": "application/pdf" },
-    body: ss.toString("base64"),
-    isBase64Encoded: true,
+    statusCode: 404,
+    body: "Only support POST method!",
   };
 };
---
+++ lib/schema/gen-pdf.ts
@@ -0,0 +1,16 @@
+import { z } from "zod";
+
+export const requestBodySchema = z.object({
+  pdfInfo: z.object({
+    suratKe: z.string().min(1),
+    tanggalPembuatan: z.string().min(1),
+    jadwalReguler: z.string().min(1),
+    waktuReguler: z.string().min(1),
+    kelas: z.array(z.object({ value: z.string().min(1) })),
+  }),
+  userInfo: z.object({
+    "next-auth.csrf-token": z.string(),
+    "next-auth.callback-url": z.string().url(),
+    "next-auth.session-token": z.string(),
+  }),
+});
